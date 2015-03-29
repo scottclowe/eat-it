5e661ffb91f791c7b0d21f9b47301eb6a12795a6
Scott Lowe <scottlowe0@gmail.com>
2015-03-30 00:40:38 +0100

    Initial tests

---
+++ train_initial.py
@@ -0,0 +1,60 @@
+import sklearn.linear_model
+import sklearn.cross_validation
+import sklearn.metrics
+import pandas as pd
+import numpy as np
+import datetime
+import csv
+
+train = pd.read_csv('data/train.csv')
+test  = pd.read_csv('data/test.csv')
+
+# Calculate the age of each sample, in days
+end_dt = datetime.datetime.strptime('2015-1-1', "%Y-%m-%d")
+train['Age'] = [(end_dt - datetime.datetime.strptime(open_dt, "%m/%d/%Y")).days for open_dt in train['Open Date']]
+test['Age']  = [(end_dt - datetime.datetime.strptime(open_dt, "%m/%d/%Y")).days for open_dt in test['Open Date']]
+
+# Take the log of the age of the venue
+X  = np.log(train[['Age']].values.reshape((train.shape[0],1)))
+Xt = np.log(test[['Age']].values.reshape((test.shape[0],1)))
+
+# The target is of course the revenue
+y  = train['revenue'].values
+
+# We will use Linear Regression as our model
+clf = sklearn.linear_model.LinearRegression()
+
+# Cross-validate
+scores=[]
+ss = sklearn.cross_validation.KFold(len(y), n_folds=3, shuffle=True, random_state=42)
+for trainCV, testCV in ss:
+    # Split into train and test
+    X_train, X_test, y_train, y_test = X[trainCV], X[testCV], y[trainCV], y[testCV]
+    # Fit the classifier
+    clf.fit(X_train, np.log(y_train))
+    # Predict the revenue
+    y_pred = np.exp(clf.predict(X_test))
+    # Compute mean squared error
+    mse = sklearn.metrics.mean_squared_error(y_test,y_pred)
+    print(mse**0.5)
+    scores.append(mse)
+
+# Average RMSE from cross validation
+scores = np.array(scores)
+print("CV Score: {}".format(np.mean(scores**0.5)))
+
+# Fit model again on the full training set
+clf.fit(X,np.log(y))
+# Predict test.csv & reverse the log transform
+yp = np.exp(clf.predict(Xt))
+
+# Write submission file
+mobile_factor = 0.90
+li = test['Type']=='MB'
+print('Found {} Mobile types'.format(sum(li)))
+yp[li] = yp[li]*mobile_factor
+
+sub = pd.DataFrame(test['Id'])
+sub['Prediction'] = yp
+sub.to_csv('sub_{}.csv'.format(mobile_factor), index=False)
+
